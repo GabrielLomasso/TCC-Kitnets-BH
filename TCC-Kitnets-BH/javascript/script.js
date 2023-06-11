919f50d1be1ac76a0e5fa2fcbc5b89efc4927b1c
GabrielLomasso <129525727+GabrielLomasso@users.noreply.github.com>
2023-06-11 19:01:35 -0300

    Delete TCC-Kitnets-BH directory

--- TCC-Kitnets-BH/javascript/script.js
+++
@@ -1,6 +0,0 @@
-//Whatsapp
-
-window.addEventListener("scroll", function(){
-    var button = document.getElementById("whatsapp");
-    button.classList.toggle("visible", window.scrollY > 100);
-});
