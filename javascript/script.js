6d633db2b13e38df4388d2bb12e538d8ba41420b
DevJohanBoli <129955638+DevJohanBoli@users.noreply.github.com>
2023-06-13 11:45:00 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -20,3 +20,23 @@ function abrir_fechar(){
     }
 }
 
+//Carrosel
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
